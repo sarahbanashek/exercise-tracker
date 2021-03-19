a2c5106fda7fd0a21f17bb42b6f2bd6c80ad88d9
Sarah Banashek <sarahmbanashek@gmail.com>
2021-03-19 16:32:25 -0700

    updating ViewAllWorkouts component with chakra ui

--- src/components/ViewAllWorkouts.js
+++ src/components/ViewAllWorkouts.js
@@ -1,28 +1,53 @@
+import { 
+  CloseButton,
+  Heading,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Stack 
+} from "@chakra-ui/react";
 import { monthDayYearDate } from '../utilities/monthDayYearDate';
 
 export function ViewAllWorkouts({listAllExerciseEvents, toggleShowExerciseEvents}) {
     return (
-      <div id="view-all-workouts-table">
-        <input type="button" className="close-form" aria-label="close the view all workouts table" value="X" onClick={() => toggleShowExerciseEvents(false, 'view')} />
-        <table>
-          <caption>Your Workouts</caption>
-          <tbody>
-            <tr key="title-row">
-                <th key="date-title" scope="col">Date</th>
-                <th key="type-title" scope="col">Workout Type</th>
-                <th key="duration-title" scope="col">Workout Length</th>
-                <th key="heart-rate-title" scope="col">Heart Rate</th>
-            </tr>
-            {listAllExerciseEvents.map(e => 
-            <tr key={e.id}>
-            <td key={'date-' + e.id}>{monthDayYearDate(e.date)}</td>
-            <td key={'type-' + e.id}>{e.description}</td>
-            <td key={'duration-' + e.id}>{e.duration} min</td>
-            <td key={'heart-rate-' + e.id}>{e.heart_rate ? `${e.heart_rate} bpm` : 'n/a'}</td>
-            </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+      <Stack
+        border="2px"
+        borderRadius="lg" 
+        borderColor="blue.400" 
+        variant="outline" 
+        paddingTop={2}>
+        <Stack paddingRight={2} direction="row-reverse">
+          <CloseButton color="blue.700" onClick={() => toggleShowExerciseEvents(false, 'view')} />
+        </Stack>
+
+        <Heading size="md">Your Workouts</Heading>
+
+        <Stack p={10}>
+          <Table variant="simple" colorScheme="blue" paddingRight={10}>
+            <Thead>
+              <Tr key="title-row">
+                  <Th key="date-title" scope="col">Date</Th>
+                  <Th key="type-title" scope="col">Workout Type</Th>
+                  <Th key="duration-title" scope="col">Workout Length</Th>
+                  <Th key="heart-rate-title" scope="col">Heart Rate</Th>
+              </Tr>
+            </Thead>
+            
+            <Tbody>  
+              {listAllExerciseEvents.map(e => 
+              <Tr key={e.id}>
+                <Td key={'date-' + e.id}>{monthDayYearDate(e.date)}</Td>
+                <Td key={'type-' + e.id}>{e.description}</Td>
+                <Td key={'duration-' + e.id}>{e.duration} min</Td>
+                <Td key={'heart-rate-' + e.id}>{e.heart_rate ? `${e.heart_rate} bpm` : 'n/a'}</Td>
+              </Tr>
+              )}
+            </Tbody>
+          </Table>
+        </Stack>
+      </Stack>
     );
 }
