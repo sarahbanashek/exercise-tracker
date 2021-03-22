905debb1d36e65de586c5519f8a0f9228f936771
Sarah Banashek <sarahmbanashek@gmail.com>
2021-03-22 15:12:06 -0700

    formatting EditExerciseTypes component

--- src/components/EditExerciseTypes.js
+++ src/components/EditExerciseTypes.js
@@ -2,61 +2,86 @@ import { useState } from 'react';
 import { 
   Button,
   CloseButton,
-  Container,
   FormControl,
   FormHelperText,
   FormLabel,
   Heading,
-  HStack,
   Input,
-  Select
+  Select,
+  Stack,
+  VStack
 } from "@chakra-ui/react"
 
 export function EditExerciseTypes({unusedExerciseTypes, editExerciseTypesInDB, setShowEditExerciseTypes}) {
-    const [idsToRemove, setIdsToRemove] = useState();
-    const [workoutsToAdd, setWorkoutsToAdd] = useState();
-  
-    let selectSize = unusedExerciseTypes.length < 3
-      ? unusedExerciseTypes.length * 25
-      : 75;
-    
-    function handleSubmit() {
-      let data = {};
-      if (idsToRemove) {
-        data.toRemove = idsToRemove;
-      }
-      if (workoutsToAdd) {
-        data.toAdd = workoutsToAdd;
-      }
-      editExerciseTypesInDB(data);
-      setShowEditExerciseTypes(false);
-    }
+  const [idsToRemove, setIdsToRemove] = useState();
+  const [workoutsToAdd, setWorkoutsToAdd] = useState();
 
-    return(
-      <Container centerContent id="edit-exercise-types-container">
-        <HStack>
-          <CloseButton onClick={() => setShowEditExerciseTypes(false)} />
-          <Heading size="md">Edit your workout types</Heading>
-        </HStack>
-        <FormControl>
-            <FormLabel>Choose which workout types to remove</FormLabel>
-            <Select multiple 
+  let selectSize = unusedExerciseTypes.length < 3
+    ? unusedExerciseTypes.length * 25
+    : 75;
+  
+  function handleSubmit() {
+    let data = {};
+    if (idsToRemove) {
+      data.toRemove = idsToRemove;
+    }
+    if (workoutsToAdd) {
+      data.toAdd = workoutsToAdd;
+    }
+    editExerciseTypesInDB(data);
+    setShowEditExerciseTypes(false);
+  }
+
+  const borderColor = 'green.400';
+  const closeButtonColor = 'green.700';
+  const colorScheme = 'green';
+  const containerBackgroundColor = 'green.50';
+  const textColor = 'green.800';
+
+  return(
+    <Stack 
+      border="2px"
+      borderRadius="lg" 
+      borderColor={borderColor} 
+      bg={containerBackgroundColor}
+      paddingTop={2}
+      paddingBottom={5}
+      marginBottom="10px"
+      >
+      <Stack paddingRight={2} direction="row-reverse">
+        <CloseButton color={closeButtonColor} onClick={() => setShowEditExerciseTypes(false)} />
+      </Stack>
+
+      <VStack spacing={7} paddingLeft={5} paddingRight={5}>
+        <Heading color={textColor} size="md">Edit workout types</Heading>
+        
+        <FormControl  align="center">
+            <FormLabel textAlign="center" color={textColor}>Choose which workout type(s) to remove</FormLabel>
+            <Select id="select-remove-workout-type"
+            multiple 
             value={idsToRemove}
-            maxWidth="45%"
+            width="fit-content"
             height={selectSize} 
+            focusBorderColor={borderColor}
+            bg="white"
+            icon="none"
             onChange={e => setIdsToRemove(Array.from(e.target.selectedOptions, option => option.value))}
             >
             {unusedExerciseTypes.map(entry => 
               <option key={entry.id} value={entry.id}>{entry.description}</option>
-            )}</Select>
-            <FormHelperText>Workouts already in use cannot be removed</FormHelperText>
+            )}
+            </Select>
+            <FormHelperText color={textColor}>Workout types already in use cannot be removed</FormHelperText>
         </FormControl>
+
         <FormControl>
-          <FormLabel>Add workout(s)</FormLabel>
-          <Input type="text" maxWidth="45%" onChange={e => setWorkoutsToAdd(e.target.value)}></Input>
-          <FormHelperText>When adding multiple, separate by a comma e.g. "rock climbing, HIIT"</FormHelperText>
+          <FormLabel textAlign="center" color={textColor}>Add workout type(s)</FormLabel>
+          <Input type="text" maxWidth="55%" focusBorderColor={borderColor} bg="white" onChange={e => setWorkoutsToAdd(e.target.value)}></Input>
+          <FormHelperText color={textColor}>When adding multiple, separate by a comma e.g. "rock climbing, HIIT"</FormHelperText>
         </FormControl>
-        <Button type="submit" onClick={() => handleSubmit()}>Submit</Button>
-      </Container>
-    );
+
+        <Button type="submit" colorScheme={colorScheme} onClick={() => handleSubmit()}>Submit</Button>
+      </VStack>
+    </Stack>
+  );
 }
