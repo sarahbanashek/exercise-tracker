b179a5c335dbc1cbc95cb6726fe27d6130ecdbc0
Sarah Banashek <sarahmbanashek@gmail.com>
2021-03-23 17:44:54 -0700

    switching color scheme to chakra default colors

--- src/utilities/colors.js
+++ src/utilities/colors.js
@@ -1,14 +1,76 @@
-export const durationColor = '#003f5c';
+// colors from chakra ui default color themes
 
-export const heartRateColor = '#a05195';
+export const durationColor = '#00B5D8';  // cyan 500
+
+export const heartRateColor = '#D53F8C';  // pink 500
+
+export const tooltipBorderColors = [
+    // pink 600
+    '#B83280',
+    // purple 600
+    '#6B46C1',
+    // cyan 600
+    '#00A3C4',
+    // blue 600
+    '#2b6cb0',
+    // teal 600
+    '#2C7A7B',
+    // green 600
+    '#2F855A',
+    // yellow 400
+    '#ECC94B',
+    // orange 500
+    '#DD6B20'
+]
+
+export const tooltipBackgroundColors = [
+    // pink 50
+    '#FFF5F7',
+    // purple 50
+    '#FAF5FF',
+    // cyan 50
+    '#EDFDFD',
+    // blue 50
+    '#ebf8ff',
+    // teal 50
+    '#E6FFFA',
+    // green 50
+    '#F0FFF4',
+    // yellow 50
+    '#FFFFF0',
+    // orange 50
+    '#FFFAF0',
+]
 
 export const pieChartColors = [
-    '#003f5c',
-    '#2f4b7c',
-    '#665191',
-    '#a05195',
-    '#d45087',
-    '#f95d6a',
-    '#ff7c43',
-    '#ffa600'
+    // pink 400
+    '#ED64A6',
+    // purple 400
+    '#9F7AEA',
+    // cyan 400
+    '#0BC5EA',
+    // blue 400
+    '#4299e1',
+    // teal 400
+    '#38B2AC',
+    // green 400
+    '#48BB78',
+    // yellow 400
+    '#ECC94B',
+    // orange 400
+    '#ED8936',
+    // pink 500
+    '#D53F8C',
+    // purple 500
+    '#805AD5',
+    // cyan 500
+    '#00B5D8',
+    // blue 500
+    '#3182ce',
+    // teal 500
+    '#319795',
+    // green 500
+    '#38A169',
+    // orange 500
+    '#DD6B20'
 ];
