a6736c71e01fa2bd2b72dae725bd70f0a22f6ad2
Sarah Banashek <sarahmbanashek@gmail.com>
2021-03-25 13:48:30 -0700

    switching DataAverages container to a Wrap

--- src/components/DataAverages.js
+++ src/components/DataAverages.js
@@ -4,40 +4,45 @@ import {
   StatNumber,
   StatHelpText,
   StatArrow,
-  StatGroup,
+  Wrap,
+  WrapItem
 } from "@chakra-ui/react"
 
 export function DataAverages({averages}) {
 
     return (
-      <StatGroup p={5}>
-        <Stat>
-          <StatLabel>Average Workout Length</StatLabel>
-          <StatNumber>{averages.totalDuration}</StatNumber>
-          <StatHelpText>minutes</StatHelpText>
-          <StatHelpText>
-            <StatArrow type={averages.totalDuration > averages.untilLastWeekDuration
-                              ? 'increase'
-                              : 'decrease'
-                            } />
-            {(100 * Math.abs(averages.totalDuration - averages.untilLastWeekDuration) / averages.untilLastWeekDuration).toFixed(2)}% this week
-          </StatHelpText>
-        </Stat>
+      <Wrap align="center" justify="center" spacing="15%" p={5}>
+        <WrapItem>
+          <Stat>
+            <StatLabel>Average Workout Length</StatLabel>
+            <StatNumber>{averages.totalDuration}</StatNumber>
+            <StatHelpText>minutes</StatHelpText>
+            <StatHelpText>
+              <StatArrow type={averages.totalDuration > averages.untilLastWeekDuration
+                                ? 'increase'
+                                : 'decrease'
+                              } />
+              {(100 * Math.abs(averages.totalDuration - averages.untilLastWeekDuration) / averages.untilLastWeekDuration).toFixed(2)}% this week
+            </StatHelpText>
+          </Stat>
+        </WrapItem>
 
-        <Stat>
-          <StatLabel>Average Heart Rate</StatLabel>
-          <StatNumber>{averages.totalHeartRate}</StatNumber>
-          <StatHelpText>
-            beats per minute
-          </StatHelpText>
-          <StatHelpText>
-            <StatArrow type={averages.totalHeartRate > averages.untilLastWeekHeartRate
-                              ? 'increase'
-                              : 'decrease'
-                            } />
-            {(100 * Math.abs(averages.totalHeartRate - averages.untilLastWeekHeartRate) / averages.untilLastWeekHeartRate).toFixed(2)}% this week
-          </StatHelpText>
-        </Stat>
-      </StatGroup>
+        <WrapItem>
+          <Stat>
+            <StatLabel>Average Heart Rate</StatLabel>
+            <StatNumber>{averages.totalHeartRate}</StatNumber>
+            <StatHelpText>
+              beats per minute
+            </StatHelpText>
+            <StatHelpText>
+              <StatArrow type={averages.totalHeartRate > averages.untilLastWeekHeartRate
+                                ? 'increase'
+                                : 'decrease'
+                              } />
+              {(100 * Math.abs(averages.totalHeartRate - averages.untilLastWeekHeartRate) / averages.untilLastWeekHeartRate).toFixed(2)}% this week
+            </StatHelpText>
+          </Stat>
+        </WrapItem>
+      </Wrap>
     );
 }
